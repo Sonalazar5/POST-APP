571d76d35ec750ee2bb12cd128e61af070cbcd70
Sonalazar5 <sonalazar5@gmail.com>
2024-06-08 16:18:18 +0530

    POST

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
-
 const Add = () => {
+    const[data,setData]=useState(
+        {
+            "name":"",
+            "dateofbirth":"",
+            "email":"",
+            "password":"",
+            "confirmpassword":""
+        }
+    )
+    const inputHandler=(event)=>{
+setData({...data,[event.target.name]:event.target.value})
+    } 
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <Navbar/>
@@ -12,27 +26,27 @@ const Add = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label htmlFor="" className="form-label">NAME</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
                 </div>
                 
                
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">DATE-OF-BIRTH</label>
-                <input type="date" name="" id="" className="form-control" />
+                <input type="date"  id="" className="form-control" name='dateofbirth' value={data.dateofbirth} onChange={inputHandler}/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label htmlFor="" className="form-label">EMAIL-ID</label>
-                <input type="text" className="form-control" /></div>
+                <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} /></div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label htmlFor="" className="form-label">PASSWORD</label>
-               <input type="password" name="" id="" className="form-control" /></div>
+               <input type="password"  id="" className="form-control" name='password' value={data.password} onChange={inputHandler} /></div>
                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <label htmlFor="" className="form-label">CONFIRM PASSWORD</label>
-                <input type="password" name="" id="" className="form-control" />
+                <label htmlFor="" className="form-label">CONFIRMPASSWORD</label>
+                <input type="password" id="" className="form-control"  name='confirmpassword' value={data.confirmpassword} onChange={inputHandler}/>
                 </div>
                 <center>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button class="btn btn-success">ADD</button>
+                    <button class="btn btn-success" onClick={readValue} >ADD</button>
                 </div>
                 </center>
             </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 
 export const Search = () => {
+    const [data,setData]=useState(
+        {
+            "name":"",
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]: event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
     <Navbar/>
@@ -12,11 +23,11 @@ export const Search = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-format">NAME</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
                         </div>
                         <center>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button class="btn btn-warning">SEARCH</button>
+                            <button class="btn btn-warning" onClick={readValue}>SEARCH</button>
                         </div>
                         </center>
                     </div>
